2e2bc722aa150170654aa96c495cee32fcc28162
Azkel1 <azk.alexis.com@gmail.com>
2021-05-02 23:22:24 +0200

    Added first iteration of block rendering. Moved canvas event handling to canvas class. Base functionality for the buttons used to add blocks to the sandbox.

--- components/canvas.js
+++ components/canvas.js
@@ -1,4 +1,5 @@
 import { Canvas, Rect } from "lib/fabric.custom"
+import { EventEmitter } from "lib/events"
 import React from "react"
 
 import styles from "components/canvas.module.scss"
@@ -20,98 +21,34 @@ export default class SandboxCanvas extends React.Component {
             selection: false
         });
 
-        document.querySelector("#renderButton").addEventListener("click", (e) => {
-            this.canvas.renderAll();
-        });
-
         window.addEventListener("resize", () => {
-            //Set height & width to 0 so the parent div can resize
+            // Set height & width to 0 so the parent div can resize
             document.querySelector(".canvas-container").style.width = 0;
             document.querySelector(".canvas-container").style.height = 0;
 
-            //Get the current width & height from the parent div. Canvas properties will update automatically
-            try { //FIXME: Sometimes it causes an exception, I don't know why. Maybe it has something to do with the browser live reload.
+            // Get the current width & height from the parent div. Canvas properties will update automatically
+            try { // FIXME: Sometimes it causes an exception, I don't know why. Maybe it has something to do with the browser live reload.
                 this.canvas.setWidth(this.canvasContainer.current.clientWidth);
                 this.canvas.setHeight(this.canvasContainer.current.clientHeight);
             } catch (e) {
-                console.log(e);
+                console.error(e);
             }
-        })
+        });
 
-        //TEMP: Testing purposes only
-        this.canvas.add(new Rect(0, undefined, "div", 400, 200));
+        EventEmitter.subscribe("createCanvasItem", (itemType, top, left) => {
+            this.canvas.addCanvasBlock(itemType, top, left);
+        });
+
+        // * Testing purposes only
+        /* this.canvas.add(new Rect(0, undefined, "div", 400, 200));
         this.canvas.add(new Rect(300, undefined, "img"));
-        this.canvas.add(new Rect(500, undefined, "h1"));
+        this.canvas.add(new Rect(500, undefined, "h1")); */
         /* ----------------------------------------- */
-
-        this.canvas.on("object:moving", (e) => {
-            if (e.target.isType("rect")) {
-                //Set variables for ease of use
-                let activeObject = e.target;
-                let target = activeObject.closestObject;
-
-                activeObject.setCoords(); // Update object coords as its moving. Used so the following calculations use the correct coordinates.
-
-                if (activeObject.closestObject === null) {  //The object isn't close to any other object.
-                    this.canvas.forEachObject((obj) => {  //Check each Rect of the canvas to see if its close enough.
-                        if (activeObject !== obj && obj.isType("rect")) {
-                            if (activeObject.intersectsWithObject(obj)) {
-                                activeObject.closestObject = obj;
-                            }
-                        }
-                    });
-                } else {  //The object has a object close enough to snap to it
-                    if (!activeObject.intersectsWithObject(target)) { //If the object leaves the range
-                        activeObject.hideSnapStyles();
-                    } else { //The object is close enough to snap
-                        target.set({ strokeWidth: 2 });
-                        activeObject.set({ strokeWidth: 2 });
-                    }
-                }
-            }
-        });
-
-        this.canvas.on("object:modified", (e) => {
-            if (e.target.isType("rect")) {
-                let activeObject = e.target;
-                let target = activeObject.closestObject;
-
-                //Check if the object has been dragged outside the canvas. (Left-Right)
-                if (activeObject.getPos().left >= (this.canvas.width - activeObject.width)) {
-                    activeObject.setPos(undefined, (this.canvas.width - activeObject.width));
-                } else if (activeObject.getPos().left < 0) {
-                    activeObject.setPos(undefined, 0);
-                }
-
-                //Check if the object has been dragged outside the canvas. (Top-Bottom)
-                if (activeObject.getPos().top >= (this.canvas.height - activeObject.height)) {
-                    activeObject.setPos((this.canvas.height - activeObject.height));
-                } else if (activeObject.getPos().top < 0) {
-                    activeObject.setPos(0);
-                }
-
-                //Snap the current object to the target, add the activeObject id to the list of childs of the parent and set the activeObject parent to the target id.
-                if (target !== null && target.isContainer) {
-                    activeObject.setPos(
-                        target.top + 10,
-                        target.left + 10 + (activeObject.width * target.childObjects.size));
-                    target.childObjects.add(activeObject.id);
-                    activeObject.parent = target.id;
-                    activeObject.hideSnapStyles();
-                } else { //Remove the childs from the parent & reset the objects parent.
-                    if (activeObject.parent !== null) {
-                        this.canvas.getObject(activeObject.parent).childObjects.delete(activeObject.id);
-                        activeObject.parent = null;
-                    }
-                }
-            }
-        });
     }
 
     render() {
         return (
             <div id={styles.canvasContainer} ref={this.canvasContainer}>
-                <button id="renderButton">Rerenderizar</button>
                 <canvas id="main-canvas"></canvas>
             </div>
         )
--- pages/sandbox.js
+++ pages/sandbox.js
@@ -1,9 +1,59 @@
+import React from "react"
 import Head from "next/head"
 import Header from "components/header"
 import SandboxCanvas from "components/canvas"
+import ToolContainer from "components/toolContainer"
+import { EventEmitter } from "lib/events"
+import { HTML_ELEMENTS } from "lib/constants"
+
 import styles from "styles/sandbox.module.scss"
 
 export default function SandboxPage () {
+    const toolContainer = React.createRef();
+    const sandboxCanvas = React.createRef();
+    const contentContainer = React.createRef();
+
+    // When the "renderHTML" event is fired, get all the elements from the canvas and create the corresponding HTML structure.
+    EventEmitter.subscribe("renderHTML", (canvas) => {
+        contentContainer.current.textContent = "";
+        
+        canvas.forEachObject((obj) => (createElement)(obj, contentContainer.current));
+
+        /**
+         * Create a HTML element from a fabric object.
+         * TODO: Find a more appropiate type for the target
+         * @param {fabric.Object} obj Object to be created & added
+         * @param {object} target Container to which the object will be appended.
+         */
+        function createElement(obj, target) {
+            if (obj.isType("rect") && !document.getElementById(obj.id)) {
+                const em = document.createElement(obj.emType);
+                const label = document.createElement("span");
+
+                em.id = obj.id;
+                em.style.backgroundColor = HTML_ELEMENTS[obj.emType].color;
+                if (obj.emType === "img") em.src = "/svg/empty_image.svg";
+                
+                label.textContent = obj.label || HTML_ELEMENTS[obj.emType].defaultLabel;
+                em.appendChild(label);
+
+                if (obj.isContainer && obj.childObjects.size > 0) {
+                    Array.from(obj.childObjects).forEach((childObj) => (createElement)(canvas.getObject(childObj), em));
+                    target.appendChild(em);
+                } else {
+                    // If the object has a parent make sure the element is being added to it
+                    if (obj.parent) {
+                        if (obj.parent === target.id) target.appendChild(em);
+                    } 
+                    // If not it means its parent is the container, add it.
+                    else {
+                        target.appendChild(em);
+                    }
+                }
+            }
+        }
+    });
+    
     return (
         <>
             <Head>
@@ -13,9 +63,9 @@ export default function SandboxPage () {
             <Header page="sandbox"/>
 
             <main id={ styles.sandboxMainContainer }>
-                <div id={ styles.toolContainer }></div> {/* TODO: Make separate component */}
-                <SandboxCanvas />
-                <div id={ styles.renderedContentContainer }></div> {/* TODO: Make separate component */}
+                <ToolContainer ref={ toolContainer }/> {/* TODO: Make separate component */}
+                <SandboxCanvas ref={ sandboxCanvas }/>
+                <div id={ styles.renderedContentContainer } ref={ contentContainer }></div> {/* TODO: Make separate component */}
             </main>
         </>
     )
